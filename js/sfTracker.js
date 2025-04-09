7446c30aabf5ab8f746b1ab1e4dcf500d1305ea8
Matthew T. Bramer <mbramer@mercer-trans.com>
2025-04-09 14:57:04 -0400

    updates

--- js/sfTracker.js
+++ js/sfTracker.js
@@ -53,24 +53,31 @@ function calculateSeizureFreeDuration(lastDate) {
   </div>
 `;
 
+
 document.body.insertAdjacentHTML('afterbegin', modalHTML);
 
 const seizureEvents = [
     {
       date: "2023-12-29",
-      title: "Final Seizure",
+      title: "Final Seizure (December 29, 2023)",
       description: "Everett experienced his last seizure on this day. A turning point in his journey!"
     },
-    // {
-    //   date: "2023-10-15",
-    //   title: "Episode",
-    //   description: "A mild seizure after waking up."
-    // },
-    // {
-    //   date: "2023-07-03",
-    //   title: "First Seizure",
-    //   description: "Everett's very first seizure."
-    // }
+    {
+      date: "2023-10-11",
+      title: "Episode (October 11, 2023)",
+      description: "3 1/2 beautiful months and we had to reset the seizure clock yesterday morning 😔 we had started to let ourselves believe that he might be growing out of it. Thankfully it was small and short but still hate to see what it does to him. Please continue to keep our warrior in your prayers 💜 #epilepsyawareness #epilepsywarrior"
+    },
+    {
+      date: "2023-07-04",
+      title: "Episode (July 4, 2023)",
+      description: "Been up since 4am because I had to use the restroom and when I came back to bed, Everett was moving/tossing a lot. So panic set in. So now I stay up to make sure he’s okay and I don’t miss a seizure. This is being an epilepsy mama. We had our longest stent of seizure free time (6 weeks) cut short last Tuesday when I was awoken at about 5:50am to my baby seizing once again. Thank you Jesus for waking me up and I pray I’ve never missed one from being asleep.It’s been 3 months since our diagnosis and he has suffered 5 seizures(including his first). It never gets easier. I’m more educated and I know what to do but every time feels like the first time. I feel helpless. Lord, please protect my child. #epilepsyawareness #mamaofawarrior"
+    },
+    {
+      date: "2023-03-29",
+      title: "First Seizure (March 26, 2023)",
+      facebookPostUrl: "https://www.facebook.com/share/p/1V2AEKhowq/",
+      description: "From Sandy (Everett's Mom). Hi everyone. We already have a lot of immediate family and if you mix in our church family, it’s much easier for me to share this here than to try to tell/text everyone individually. Friday afternoon, Everett woke up from a nap and was immediately unresponsive to me. Still breathing but head was locked to the side, eyes glazed over, and not responding to anything. Immediately I knew this was a seizure. We called for an ambulance and when we got to Kosair, he had another seizure. This time, just like the ones you see in movies, with convulsions and twitching. He finally stopped after the doctors gave him three rounds of medicine. This was an hour total of two seizures and being in an unresponsive postictal state. We were at the hospital for the next 24 hours. He had blood work, CT scan, EEG, and an MRI. His blood work was clean meaning he didn’t get into any medicine or anything to make him seize. He did not have a fever so it was not a febrile seizure. He has no allergies aside from seasonal. There is purely no known cause for what happened. Therefore, our sweet, beautiful, perfect baby boy was diagnosed with Epilepsy. His EEG showed that his brain is making a lot of potential seizure activity with no cause. Our miracle is that after seizing for way, way too long… his CT and MRI came back normal and the seizures did not seem to do any brain damage. My baby was been through so much the past three days and he is just worn out completely. Matt and I are beside ourselves with anxiety and worry as to what our life may look like now as we are now always on watch for a seizure with no known triggers at this time. I’m absolutely terrified of seeing my son go through that again.. I honestly don’t have a lot of words right now. I am just asking for anyone who believes in prayer, to put my boy in your prayers please. He does not deserve to go through this. And please, hug your babies for me. NOBODY deserves to see their happy, carefree child like this.. ."
+    }
   ];
   
   const timelineContainer = document.getElementById("timeline");
@@ -101,3 +108,5 @@ const seizureEvents = [
     }
   };
   
+
+
